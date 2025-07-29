f9e2462374f3c679043c6b6a3c7475acca79b093
MickeyH@Yoga2 <hozmickey@gmail.com>
2025-07-29 22:11:23 +0300

    added bounds checking to movement and improved key-up detection

--- sdl-scene/input.cpp
+++ sdl-scene/input.cpp
@@ -1,7 +1,11 @@
 #include "input.hpp"
 #include "transform.hpp"
 
-static constexpr int DELAY_MS = (32);
+#include <queue>
+#include <set>
+
+static constexpr int POLLING_DELAY_MS = (32);
+static constexpr int COOLDOWN_DELAY_MS = (32);
 
 static bool is_initialized = false;
 
@@ -16,48 +20,57 @@ static InputId input_poll(void)
     static SDL_Keycode latest_keycode = SDLK_UNKNOWN;
     static InputId latest_input_id = INPUT_NONE;
 
-    if (SDL_PollEvent(&event))
+    static std::set<SDL_Keycode> key_up_set;
+
+    key_up_set.clear();
+
+    while (SDL_PollEvent(&event))
     {
         switch (event.type)
         {
             case SDL_KEYUP:
-                if (latest_keycode == event.key.keysym.sym)
-                {
-                    latest_keycode = SDLK_UNKNOWN;
-                    latest_input_id = INPUT_NONE;
-                }
+                key_up_set.insert(event.key.keysym.sym);
                 break;
             case SDL_QUIT:
             case SDL_APP_TERMINATING:
                 latest_input_id = INPUT_QUIT;
                 break;
             case SDL_KEYDOWN:
-                latest_keycode = event.key.keysym.sym;
-                switch(latest_keycode)
+                switch(event.key.keysym.sym)
                 {
                     case SDLK_a:
+                        latest_keycode = event.key.keysym.sym;
                         latest_input_id = INPUT_LEFT;
                         break;
                     case SDLK_w:
+                        latest_keycode = event.key.keysym.sym;
                         latest_input_id = INPUT_UP;
                         break;
                     case SDLK_d:
+                        latest_keycode = event.key.keysym.sym;
                         latest_input_id = INPUT_RIGHT;
                         break;
                     case SDLK_s:
+                        latest_keycode = event.key.keysym.sym;
                         latest_input_id = INPUT_DOWN;
                         break;
                     case SDLK_q:
+                        latest_keycode = event.key.keysym.sym;
                         latest_input_id = INPUT_QUIT;
                         break;
                     default:
-                        latest_input_id = INPUT_NONE;
                         break;
                 }
                 break;
             default:
                 break;
         }
+    }
+
+    if (key_up_set.find(latest_keycode) != key_up_set.end())
+    {
+        latest_keycode = SDLK_UNKNOWN;
+        latest_input_id = INPUT_NONE;
     }
 
     return latest_input_id;
@@ -79,22 +92,26 @@ int input_task(void *arg)
 
     while (!should_terminate)
     {
-        SDL_Delay(DELAY_MS);
+        SDL_Delay(POLLING_DELAY_MS);
         last_input = input_poll();
 
         switch(last_input)
         {
         case INPUT_LEFT:
             element->MovePosition(-1, 0);
+            SDL_Delay(COOLDOWN_DELAY_MS);
             break;
         case INPUT_UP:
             element->MovePosition(0, -1);
+            SDL_Delay(COOLDOWN_DELAY_MS);
             break;
         case INPUT_RIGHT:
             element->MovePosition(1, 0);
+            SDL_Delay(COOLDOWN_DELAY_MS);
             break;
         case INPUT_DOWN:
             element->MovePosition(0, 1);
+            SDL_Delay(COOLDOWN_DELAY_MS);
             break;
         case INPUT_QUIT:
             should_terminate = true;
--- sdl-scene/transform.cpp
+++ sdl-scene/transform.cpp
@@ -1,4 +1,5 @@
 #include "transform.hpp"
+#include "scene.hpp"
 
 Transform::Transform(void)
     : m_position{ 0 , 0 }, m_scale{ 1.0f, 1.0f }
@@ -12,8 +13,13 @@ void Transform::SetPosition(int x, int y)
 
 void Transform::MovePosition(int x, int y)
 {
-    m_position.x += x;
-    m_position.y +=y;
+    Vector2Int new_position = { m_position.x + x, m_position.y + y };
+
+    if (new_position.x < 0 || new_position.y < 0) return;
+    Vector2Int scene_size = scene_get_current()->GetSize();
+    if (new_position.x >= scene_size.x || new_position.y >= scene_size.y) return;
+
+    m_position= new_position;
 }
 
 void Transform::SetScale(float x, float y)

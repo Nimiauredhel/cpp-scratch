3d66f6a8cf0f9e06af2936c17c32131d511065f6
MickeyH@Yoga2 <hozmickey@gmail.com>
2025-07-30 15:03:03 +0300

    put the variable room size to use

--- sdl-scene/main.cpp
+++ sdl-scene/main.cpp
@@ -4,30 +4,28 @@
 #include "scene.hpp"
 #include "player.hpp"
 
-static Vector2Int scene_size = { 16, 16 };
-
 int main(void)
 {
     SDL_Thread *gfx_thread = SDL_CreateThread(gfx_task, "gfx_thread", nullptr);
 
     while(!gfx_is_initialized()) SDL_Delay(10);
 
-    for (int i = 0; i < 4; i++)
-    {
-        scene_add_new(scene_size);
-    }
+    scene_add_new({10, 2});
+    scene_add_new({2, 20});
+    scene_add_new({20, 4});
+    scene_add_new({8, 8});
 
-    scene_get_by_idx(0)->CreateDoor(8, 0, 1, 3);
-    scene_get_by_idx(0)->CreateDoor(8, 8, 0, 1);
+    scene_get_by_idx(0)->CreateDoor(0, 1, 1, 3);
+    scene_get_by_idx(0)->CreateDoor(9, 0, 0, 1);
 
-    scene_get_by_idx(1)->CreateDoor(0, 8, 1, 0);
-    scene_get_by_idx(1)->CreateDoor(0, 12, 0, 2);
+    scene_get_by_idx(1)->CreateDoor(1, 0, 1, 0);
+    scene_get_by_idx(1)->CreateDoor(1, 19, 0, 2);
 
-    scene_get_by_idx(2)->CreateDoor(5, 4, 1, 1);
-    scene_get_by_idx(2)->CreateDoor(8, 4, 0, 3);
+    scene_get_by_idx(2)->CreateDoor(0, 1, 1, 1);
+    scene_get_by_idx(2)->CreateDoor(19, 0, 0, 3);
 
-    scene_get_by_idx(3)->CreateDoor(3, 2, 1, 2);
-    scene_get_by_idx(3)->CreateDoor(7, 4, 0, 0);
+    scene_get_by_idx(3)->CreateDoor(0, 1, 1, 2);
+    scene_get_by_idx(3)->CreateDoor(6, 6, 0, 0);
 
     scene_set_current(0);
 
